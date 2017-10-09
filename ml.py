65a6a6acef383833dc5f1e6a5bbba1525f05c74b
Martin Dario Cernadas <martindario.cernadas@bbva.com>
2017-10-09 19:21:16 -0300

    Update

--- ml.py
+++ ml.py
@@ -7,7 +7,7 @@ import json
 # Create an instance with your MercadoPago credentials (CLIENT_ID and CLIENT_SECRET): 
 # Argentina: https://www.mercadopago.com/mla/herramientas/aplicaciones 
 # Brasil: https://www.mercadopago.com/mlb/ferramentas/aplicacoes
-mp = mercadopago.MP("5643352584449553", "kM7TSzfwksQPaUBi22VVEa3P97D2kXNW")
+mp = mercadopago.MP("...", "...")
 
 filters = {
         "site_id": "MLA", # Argentina: MLA; Brasil: MLB
